000e3bd79c866bc2c90c80cefe79b1a7397220b0
st-tse <st624@cornell.edu>
2021-10-15 16:35:16 -0400

    changed env and game params

--- csrc/kuhn_poker/recursive_solving.h
+++ csrc/kuhn_poker/recursive_solving.h
@@ -22,87 +22,92 @@ Recursive training and evaluation.
 #include <random>
 #include <vector>
 
-#include "liars_dice.h"
+#include "kuhn_poker.h"
 #include "net_interface.h"
 #include "subgame_solving.h"
 
-namespace liars_dice {
+namespace kuhn_poker
+{
 
-struct RecursiveSolvingParams {
-  int num_dice;
-  int num_faces;
-  // Probability to explore random action for BR player.
-  float random_action_prob = 1.0;
-  bool sample_leaf = false;
-  SubgameSolvingParams subgame_params;
-};
+  struct RecursiveSolvingParams
+  {
+    int num_dice;
+    int num_faces;
+    // Probability to explore random action for BR player.
+    float random_action_prob = 1.0;
+    bool sample_leaf = false;
+    SubgameSolvingParams subgame_params;
+  };
 
-class RlRunner {
- public:
-  RlRunner(const RecursiveSolvingParams& params, std::shared_ptr<IValueNet> net,
-           int seed)
-      : game_(Game(params.num_dice, params.num_faces)),
-        subgame_params_(params.subgame_params),
-        random_action_prob_(params.random_action_prob),
-        sample_leaf_(params.sample_leaf),
-        net_(net),
-        gen_(seed) {}
+  class RlRunner
+  {
+  public:
+    RlRunner(const RecursiveSolvingParams &params, std::shared_ptr<IValueNet> net,
+             int seed)
+        : game_(Game(params.num_dice, params.num_faces)),
+          subgame_params_(params.subgame_params),
+          random_action_prob_(params.random_action_prob),
+          sample_leaf_(params.sample_leaf),
+          net_(net),
+          gen_(seed) {}
 
-  // Deprecated constructor.
-  RlRunner(const Game& game, const SubgameSolvingParams& params,
-           std::shared_ptr<IValueNet> net, int seed)
-      : RlRunner(build_params(game, params), net, seed) {}
+    // Deprecated constructor.
+    RlRunner(const Game &game, const SubgameSolvingParams &params,
+             std::shared_ptr<IValueNet> net, int seed)
+        : RlRunner(build_params(game, params), net, seed) {}
 
-  void step();
+    void step();
 
- private:
-  static RecursiveSolvingParams build_params(
-      const Game& game, const SubgameSolvingParams& fp_params) {
-    RecursiveSolvingParams params;
-    params.subgame_params = fp_params;
-    params.num_dice = game.num_dice;
-    params.num_faces = game.num_faces;
-    return params;
-  }
+  private:
+    static RecursiveSolvingParams build_params(
+        const Game &game, const SubgameSolvingParams &fp_params)
+    {
+      RecursiveSolvingParams params;
+      params.subgame_params = fp_params;
+      //TODO
+      // params.num_dice = game.num_dice;
+      // params.num_faces = game.num_faces;
+      return params;
+    }
 
-  // Samples new state_ from the solver and update beliefs.
-  void sample_state(const ISubgameSolver* solver);
-  void sample_state_single(const ISubgameSolver* solver);
-  void sample_state_to_leaf(const ISubgameSolver* solver);
+    // Samples new state_ from the solver and update beliefs.
+    void sample_state(const ISubgameSolver *solver);
+    void sample_state_single(const ISubgameSolver *solver);
+    void sample_state_to_leaf(const ISubgameSolver *solver);
 
-  // Owning all small resources.
-  const Game game_;
-  const SubgameSolvingParams subgame_params_;
-  const float random_action_prob_;
-  const bool sample_leaf_;
-  std::shared_ptr<IValueNet> net_;
+    // Owning all small resources.
+    const Game game_;
+    const SubgameSolvingParams subgame_params_;
+    const float random_action_prob_;
+    const bool sample_leaf_;
+    std::shared_ptr<IValueNet> net_;
 
-  // Current state.
-  PartialPublicState state_;
-  // Buffer to the beliefs.
-  Pair<std::vector<double>> beliefs_;
+    // Current state.
+    PartialPublicState state_;
+    // Buffer to the beliefs.
+    Pair<std::vector<double>> beliefs_;
 
-  std::mt19937 gen_;
-};
+    std::mt19937 gen_;
+  };
 
-// Compute strategy by recursively solving subgames. Use only the strategy at
-// root of the same for the full tree, and proceed to its children.
-TreeStrategy compute_strategy_recursive(
-    const Game& game, const SubgameSolvingParams& subgame_params,
-    std::shared_ptr<IValueNet> net);
-// Compute strategy by recursively solving subgames. Use strategy for all
-// non-leaf subgame nodes as for full game strategy and proceed with leaf nodes
-// in the subgame.
-TreeStrategy compute_strategy_recursive_to_leaf(
-    const Game& game, const SubgameSolvingParams& subgame_params,
-    std::shared_ptr<IValueNet> net);
-// Compute strategy by recursively solving subgames in way that mimics training:
-// 1. Sample random iteration with linear weigting.
-// 2. Copy the sampling strategy for the solver to the full game strategy.
-// 3. Compute beliefs in leaves using belief_propogation_strategy start
-// recursively.
-TreeStrategy compute_sampled_strategy_recursive_to_leaf(
-    const Game& game, const SubgameSolvingParams& subgame_params,
-    std::shared_ptr<IValueNet> net, int seed, bool root_only = false);
+  // Compute strategy by recursively solving subgames. Use only the strategy at
+  // root of the same for the full tree, and proceed to its children.
+  TreeStrategy compute_strategy_recursive(
+      const Game &game, const SubgameSolvingParams &subgame_params,
+      std::shared_ptr<IValueNet> net);
+  // Compute strategy by recursively solving subgames. Use strategy for all
+  // non-leaf subgame nodes as for full game strategy and proceed with leaf nodes
+  // in the subgame.
+  TreeStrategy compute_strategy_recursive_to_leaf(
+      const Game &game, const SubgameSolvingParams &subgame_params,
+      std::shared_ptr<IValueNet> net);
+  // Compute strategy by recursively solving subgames in way that mimics training:
+  // 1. Sample random iteration with linear weigting.
+  // 2. Copy the sampling strategy for the solver to the full game strategy.
+  // 3. Compute beliefs in leaves using belief_propogation_strategy start
+  // recursively.
+  TreeStrategy compute_sampled_strategy_recursive_to_leaf(
+      const Game &game, const SubgameSolvingParams &subgame_params,
+      std::shared_ptr<IValueNet> net, int seed, bool root_only = false);
 
-}  // namespace liars_dice
+} // namespace kuhn_poker
