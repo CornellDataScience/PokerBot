dd763965b66ea0634dfa1c5686ae8d029f70b8d0
st-tse <st624@cornell.edu>
2021-10-22 15:56:09 -0400

    modified for kuhn poker

--- csrc/kuhn_poker/gen_benchmark.cc
+++ csrc/kuhn_poker/gen_benchmark.cc
@@ -29,31 +29,37 @@
 #include "subgame_solving.h"
 #include "util.h"
 
-using namespace liars_dice;
+using namespace kuhn_poker;
 using namespace rela;
 
-int get_depth(const Tree& tree, int root = 0) {
+int get_depth(const Tree &tree, int root = 0)
+{
   int depth = 1;
-  for (auto child : ChildrenIt(tree[root])) {
+  for (auto child : ChildrenIt(tree[root]))
+  {
     depth = std::max(depth, 1 + get_depth(tree, child));
   }
   return depth;
 }
 
-struct Timer {
+struct Timer
+{
   std::chrono::time_point<std::chrono::system_clock> start =
       std::chrono::system_clock::now();
 
-  double tick() {
+  double tick()
+  {
     const auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> diff = end - start;
     return diff.count();
   }
 };
 
-int main(int argc, char* argv[]) {
-  int num_dice = 1;
-  int num_faces = 4;
+int main(int argc, char *argv[])
+{
+  // int num_dice = 1;
+  // int num_faces = 4;
+  int deck_size = 3;
   int fp_iters = 1024;
   int mdp_depth = 2;
   int num_threads = 10;
@@ -62,49 +68,63 @@ int main(int argc, char* argv[]) {
   std::string device = "cuda:1";
   std::string net_path;
   {
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
       std::string arg = argv[i];
-      if (arg == "--num_dice") {
+      if (arg == "--deck_size")
+      {
         assert(i + 1 < argc);
-        num_dice = std::stoi(argv[++i]);
-      } else if (arg == "--num_faces") {
-        assert(i + 1 < argc);
-        num_faces = std::stoi(argv[++i]);
-      } else if (arg == "--fp_iters") {
+        deck_size = std::stoi(argv[++i]);
+      }
+      else if (arg == "--fp_iters")
+      {
         assert(i + 1 < argc);
         fp_iters = std::stoi(argv[++i]);
-      } else if (arg == "--mdp_depth") {
+      }
+      else if (arg == "--mdp_depth")
+      {
         assert(i + 1 < argc);
         mdp_depth = std::stoi(argv[++i]);
-      } else if (arg == "--num_threads") {
+      }
+      else if (arg == "--num_threads")
+      {
         assert(i + 1 < argc);
         num_threads = std::stoi(argv[++i]);
-      } else if (arg == "--per_gpu") {
+      }
+      else if (arg == "--per_gpu")
+      {
         assert(i + 1 < argc);
         per_gpu = std::stoi(argv[++i]);
-      } else if (arg == "--num_cycles") {
+      }
+      else if (arg == "--num_cycles")
+      {
         assert(i + 1 < argc);
         num_cycles = std::stoi(argv[++i]);
-      } else if (arg == "--device") {
+      }
+      else if (arg == "--device")
+      {
         assert(i + 1 < argc);
         device = argv[++i];
-      } else if (arg == "--net") {
+      }
+      else if (arg == "--net")
+      {
         assert(i + 1 < argc);
         net_path = argv[++i];
-      } else {
+      }
+      else
+      {
         std::cerr << "Unknown flag: " << arg << "\n";
         return -1;
       }
     }
   }
-  assert(num_dice != -1);
-  assert(num_faces != -1);
+  assert(deck_size != -1);
   assert(mdp_depth != -1);
 
-  const Game game(num_dice, num_faces);
+  const Game game(deck_size);
   assert(mdp_depth > 0);
   assert(!net_path.empty());
-  std::cout << "num_dice=" << num_dice << " num_faces=" << num_faces << "\n";
+  std::cout << "deck_size=" << deck_size < < < < "\n";
   {
     const auto full_tree = unroll_tree(game);
     std::cout << "Tree of depth " << get_depth(full_tree) << " has "
@@ -112,14 +132,16 @@ int main(int argc, char* argv[]) {
   }
 
   std::vector<TorchJitModel> models;
-  for (int i = 0; i < per_gpu; ++i) {
+  for (int i = 0; i < per_gpu; ++i)
+  {
     auto module = torch::jit::load(net_path);
     module.eval();
     module.to(device);
     models.push_back(module);
   }
-  std::vector<TorchJitModel*> model_ptrs;
-  for (int i = 0; i < per_gpu; ++i) {
+  std::vector<TorchJitModel *> model_ptrs;
+  for (int i = 0; i < per_gpu; ++i)
+  {
     model_ptrs.push_back(&models[i]);
   }
   auto locker = std::make_shared<ModelLocker>(model_ptrs, device);
@@ -128,13 +150,13 @@ int main(int argc, char* argv[]) {
   auto context = std::make_shared<Context>();
 
   RecursiveSolvingParams cfg;
-  cfg.num_dice = num_dice;
-  cfg.num_faces = num_faces;
+  cfg.deck_size = deck_size;
   cfg.subgame_params.num_iters = fp_iters;
   cfg.subgame_params.linear_update = true;
   cfg.subgame_params.optimistic = false;
   cfg.subgame_params.max_depth = mdp_depth;
-  for (int i = 0; i < num_threads; ++i) {
+  for (int i = 0; i < num_threads; ++i)
+  {
     const int seed = i;
     auto connector = std::make_shared<CVNetBufferConnector>(locker, replay);
     std::shared_ptr<ThreadLoop> loop =
@@ -144,7 +166,8 @@ int main(int argc, char* argv[]) {
   std::cout << "Starting the context" << std::endl;
   context->start();
   Timer t;
-  for (int i = 0; i < num_cycles; ++i) {
+  for (int i = 0; i < num_cycles; ++i)
+  {
     std::this_thread::sleep_for(std::chrono::seconds(10));
     double secs = t.tick();
     auto added = replay->numAdd();
