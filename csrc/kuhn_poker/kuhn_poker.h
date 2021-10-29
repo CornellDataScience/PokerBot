a22ac0ad7f8d5d83a567f36339239f2753cec412
st-tse <st624@cornell.edu>
2021-10-29 17:23:24 -0400

    added betting to env and pot and stack to state

--- csrc/kuhn_poker/kuhn_poker.h
+++ csrc/kuhn_poker/kuhn_poker.h
@@ -21,100 +21,131 @@
 #include <string>
 #include <vector>
 
-namespace kuhn_poker {
+namespace kuhn_poker
+{
 
-using Action = int;
+  using Action = int;
 
-// Public state of the game without tracking the history of the game.
-struct PartialPublicState {
-  // Previous call.
-  //0: fold, 1: check, 2: call, 3: bet
-  Action last_action;
-  // Player to make move next.
-  int player_id;
-  double relative_pot;
-  int starting_player;
+  // Public state of the game without tracking the history of the game.
+  struct PartialPublicState
+  {
+    // Previous call.
+    //0: fold, 1: check, 2: call, 3: bet
+    Action last_action;
+    // Player to make move next.
+    int player_id;
+    double relative_pot;
+    int starting_player;
+    std::pair<int, int> community_pot;
+    std::pair<int, int> stack;
 
-  bool operator==(const PartialPublicState& state) const {
-    return last_action == state.last_action && player_id == state.player_id
-    && relative_pot == state.relative_pot && starting_player == state.starting_player;
-  }
-};
+    bool operator==(const PartialPublicState &state) const
+    {
+      return last_action == state.last_action && player_id == state.player_id && relative_pot == state.relative_pot && starting_player == state.starting_player;
+    }
+  };
 
-class Game {
- public:
-  const int deck_size;
-  std::pair<int, int> community_pot;
-  std::pair<int, int> stack;  // how much each person has
-  //const int starting_player;
+  class Game
+  {
+  public:
+    const int deck_size;
+    std::pair<int, int> starting_community_pot;
+    std::pair<int, int> starting_stack;
+    //const int starting_player;
 
-  Game(int deck_size, std::pair<int, int> community_pot, std::pair<int, int> stack)
-      : deck_size(deck_size),
-        num_actions_(4),
-        num_hands_(deck_size),
-        community_pot(community_pot),
-        stack(stack) {}
+    Game(int deck_size, std::pair<int, int> community_pot, std::pair<int, int> stack)
+        : deck_size(deck_size),
+          num_actions_(4),
+          num_hands_(deck_size),
+          starting_community_pot(community_pot),
+          starting_stack(stack)
+    {
+    }
 
-  // Number of dice for all the players.
-  int get_deck_size() const { return deck_size; }
-  // Maximum number of distinct actions in every node.
-  Action num_actions() const { return num_actions_; }
-  // Number of distrinct game states at the beginning of the game. In other
-  // words, number of different realization of the chance nodes.
-  int num_hands() const { return num_hands_; }
-  // Community pot(ante)
-  std::pair<int, int> get_community_pot() const{ return community_pot; }
-  //stack
-  std::pair<int, int> get_stack() const{ return stack; }
-  // Upper bound for how deep game tree could be.
-  int max_depth() const { return 3; }
+    // Number of dice for all the players.
+    int get_deck_size() const { return deck_size; }
+    // Maximum number of distinct actions in every node.
+    Action num_actions() const { return num_actions_; }
+    // Number of distrinct game states at the beginning of the game. In other
+    // words, number of different realization of the chance nodes.
+    int num_hands() const { return num_hands_; }
+    // Community pot(ante)
+    // std::pair<int, int> get_community_pot() { return community_pot; }
+    // //stack
+    // std::pair<int, int> get_stack() { return stack; }
+    // Upper bound for how deep game tree could be.
+    int max_depth() const { return 3; }
 
-  PartialPublicState get_initial_state() const {
-    PartialPublicState state;
-    state.last_action = kInitialAction;
-    state.player_id = 0;
-    state.starting_player = 0;
-    return state;
-  }
+    PartialPublicState get_initial_state() const
+    {
+      PartialPublicState state;
+      state.last_action = kInitialAction;
+      state.player_id = 0;
+      state.starting_player = 0;
+      state.community_pot = starting_community_pot;
+      state.stack = starting_stack;
+      return state;
+    }
 
-  // Get range of possible actions in the state as [min_action, max_action).
-  // 0 = fold
-  // 1 = call
-  // 2 = check
-  // 3 = bet
-  std::pair<int,int> get_action_list(
-      const PartialPublicState& state) const {
-	  if (!is_terminal(state)) {
-		  return state.last_action == kInitialAction || (state.starting_player == 1-state.player_id && state.last_action == 1)
-			  ? std::pair<int, int>(2, 4)
-			  : std::pair<int, int>(0, 2);
-	  } else {
-		  return std::pair<int, int>(2,2);
-	  }
-  }
+    // Get range of possible actions in the state as [min_action, max_action).
+    // 0 = fold
+    // 1 = call
+    // 2 = check
+    // 3 = bet
+    std::pair<int, int> get_action_list(
+        const PartialPublicState &state) const
+    {
+      if (!is_terminal(state))
+      {
+        return state.last_action == kInitialAction || (state.starting_player == 1 - state.player_id && state.last_action == 1)
+                   ? std::pair<int, int>(2, 4)
+                   : std::pair<int, int>(0, 2);
+      }
+      else
+      {
+        return std::pair<int, int>(2, 2);
+      }
+    }
 
-  bool is_terminal(const PartialPublicState& state) const {
-    return ((state.last_action == 0) || ((state.last_action == 2) && (state.player_id == 1-state.starting_player))) || state.last_action == 1;
-  }
+    bool is_terminal(const PartialPublicState &state) const
+    {
+      return ((state.last_action == 0) || ((state.last_action == 2) && (state.player_id == 1 - state.starting_player))) || state.last_action == 1;
+    }
 
-  PartialPublicState act(const PartialPublicState& state, Action action) const {
-    const auto action_list = get_action_list(state);
-    assert (action == action_list.first || action == (action_list.second-1));
-    PartialPublicState new_state;
-    new_state.last_action = action;
-    new_state.player_id = 1 - state.player_id;
-    return new_state;
-  }
+    PartialPublicState act(const PartialPublicState &state, Action action) const
+    {
+      const auto action_list = get_action_list(state);
+      assert(action == action_list.first || action == (action_list.second - 1));
+      PartialPublicState new_state;
+      new_state.last_action = action;
+      new_state.community_pot = state.community_pot;
+      new_state.stack = state.stack;
 
-  std::string action_to_string(Action action) const;
-  std::string state_to_string(const PartialPublicState& state) const;
+      //betting and state
+      if (action == 1 || action == 3)
+      {
+        if (state.player_id == 0)
+        {
+          new_state.community_pot.first += 1;
+          new_state.stack.first -= 1;
+        }
+        else
+        {
+          new_state.community_pot.second += 1;
+          new_state.stack.second -= 1;
+        }
+      }
+      new_state.player_id = 1 - state.player_id;
+      return new_state;
+    }
 
- private:
+    std::string action_to_string(Action action) const;
+    std::string state_to_string(const PartialPublicState &state) const;
 
-  static constexpr int kInitialAction = -1;
-  const Action num_actions_;
-  const int num_hands_;
+  private:
+    static constexpr int kInitialAction = -1;
+    const Action num_actions_;
+    const int num_hands_;
+  };
 
-};
-
-}  // namespace kuhn_poker
+} // namespace kuhn_poker
